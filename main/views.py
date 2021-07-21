779b458d0e650f13a498e5dda7ce8480c1b58dcf
Suyog-23 <suyog231002@gmail.com>
2021-07-21 22:41:11 +0530

    konvo first push

---
+++ main/views.py
@@ -0,0 +1,10 @@
+from django.shortcuts import redirect, render
+from django.contrib.auth import logout
+
+# Create your views here.
+def homepage(request):
+    return render(request, 'main/home.html')
+
+def logout_view(request):
+    logout(request)
+    redirect('home')
